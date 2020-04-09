6895a1a8af9a8125698edf9f45e268685a7cf2f7
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-04-09 17:06:46 +0300

    .

---
+++ PoldyCvUI/BlazorApp3/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PoldyCvUI.Models
+{
+    public class RegisterModel
+    {
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Rank { get; set; }
+        public bool Status { get; set; }
+    }
+}
