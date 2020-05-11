ad8874d8bf01877d280d753f95557ec2d98ceafb
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-05-11 17:27:56 +0300

    update navbar

--- src/components/auth/NavbarAdmin.js
+++ src/components/auth/NavbarAdmin.js
@@ -10,29 +10,55 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
-export default class NavbarAdmin extends React.Component {
-  render() {
+
+class NavbarAdmin extends React.Component {
+  AuthNavi() {
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand>Poldy İnsan Kaynakları</NavbarBrand>
-          <NavbarToggler />
-          <Collapse navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink>
-                  <Link to="/login">Giriş Yap</Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>
-                  <Link to="/register">Kayıt Ol</Link>
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>Poldy İnsan Kaynakları</NavbarBrand>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to="/login">Giriş Yap</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="/register">Kayıt Ol</Link>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     );
   }
+  AdminNavi(){
+    return(
+      <div></div>
+    );
+  }
+  UserNavi(){
+    return(
+      <div></div>
+    );
+  }
+
+
+  render() {
+    
+      if (localStorage.getItem("rank")==="true") {
+        return this.AdminNavi();
+      }
+      else if(localStorage.getItem("rank")==="false"){
+        return this.UserNavi();
+      }
+      else{
+        return this.AuthNavi();
+      }
+
+  }
 }
+
+export default NavbarAdmin;
