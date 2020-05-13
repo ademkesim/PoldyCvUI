ea98837eb78692e5d8bd2d9e4e680cba9a34377a
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-05-13 15:06:18 +0300

    update

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,12 +1,6 @@
 export default {
-<<<<<<< HEAD
-    applies:[],
-    cv:{}
-  };
-  
-=======
   applies: [],
   departments: [],
   titles: [],
+  cv:{},
 };
->>>>>>> 3d1f3a182856f88b1ec46ff8e199e85bf9cbd51d
