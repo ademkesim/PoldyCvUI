cf0085573c93ee553b6f22d64763edb06743008c
kaktimurf <57290668+kaktimurf@users.noreply.github.com>
2020-05-13 17:22:46 +0300

    düzenleme yapıldı
    bir kaç küçük düzenleme

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import authReducer from "./authReducer";
 import applyListReducer from "./applyListReducer";
 import departmentListReducer from "./departmentListReducer";
 import titleListReducer from "./titleListReducer";
+import userAddCvReducer from "./userAddCvReducer"
 
 const rootReducer = combineReducers({
   auth: authReducer,
   applyListReducer,
   departmentListReducer,
   titleListReducer,
+  userAddCvReducer
 });
 
 export default rootReducer;
